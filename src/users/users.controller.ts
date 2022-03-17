218cbcf1d18c2a1877f25b0b456151fc2f9dd57c
lioartoil <a.chanakant@gmail.com>
2022-03-17 23:08:15 +0700

    Add Validation Pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('signup')
+  async createUser(@Body() body: CreateUserDto) {
+    console.debug(body);
+
+    return this.usersService.createUser(body);
+  }
+
+  async findUser() {
+    return this.usersService.findUser();
+  }
+
+  async findAllUsers() {
+    return this.usersService.find();
+  }
+
+  async updateUser() {
+    return this.usersService.update();
+  }
+
+  async removeUser() {
+    return this.usersService.remove();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateUserDto } from './dtos/create-user.dto';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async createUser(request: CreateUserDto) {}
+
+  async findUser() {}
+
+  async find() {}
+
+  async update() {}
+
+  async remove() {}
+}
