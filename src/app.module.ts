2ca518396312a87e29b64c9d4ea424926b1b09ab
lioartoil <a.chanakant@gmail.com>
2022-03-17 22:44:34 +0700

    Add Report Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Report } from './reports/report.entity';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
@@ -13,7 +14,7 @@ import { UsersModule } from './users/users.module';
     ReportsModule,
     TypeOrmModule.forRoot({
       database: 'db.sqlite',
-      entities: [User],
+      entities: [Report, User],
       synchronize: true,
       type: 'sqlite',
     }),
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Report } from './report.entity';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 
 @Module({
   controllers: [ReportsController],
+  imports: [TypeOrmModule.forFeature([Report])],
   providers: [ReportsService],
 })
 export class ReportsModule {}
