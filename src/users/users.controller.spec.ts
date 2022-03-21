36f24bdc0f545ae2794b9d91085e5d18e6ad18c9
lioartoil <a.chanakant@gmail.com>
2022-03-21 22:21:43 +0700

    Add Fake Services

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
 
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeAuthService: Partial<AuthService>;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
+    fakeAuthService = { signIn: () => {}, signUp: () => {} };
+    fakeUsersService = { find: () => {}, findOne: () => {}, remove: () => {}, update: () => {} };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
