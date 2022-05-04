338d13a928c6888bbb18d56df794c261c1a5c6f8
lioartoil <a.chanakant@gmail.com>
2022-05-04 19:36:43 +0700

    Implement Create Report Service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './report.entity';
 
 @Injectable()
 export class ReportsService {
-  async create() {}
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  async create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+
+    return this.repo.save(report);
+  }
 }
