516550c15b45955fda4c381342cca14a2cdc45da
lioartoil <a.chanakant@gmail.com>
2022-05-04 19:08:46 +0700

    Add `setupFilesAfterEnv`

---
+++ test/setup.ts
@@ -0,0 +1,18 @@
+import { rm } from 'fs/promises';
+import { join } from 'path';
+
+import { getConnection } from 'typeorm';
+
+global.beforeEach(async () => {
+  try {
+    await rm(join(__dirname, '..', 'test.sqlite'));
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+global.afterEach(async () => {
+  const connection = getConnection();
+
+  await connection.close();
+});
